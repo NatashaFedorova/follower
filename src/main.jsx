f53b883d5d640e102441a9a2f5af155af55accb5
Наташа Федорова <98808643+NatashaFedorova@users.noreply.github.com>
2023-05-30 16:33:02 +0300

    fix: basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter base='follower'>
+    <BrowserRouter basename='follower'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
