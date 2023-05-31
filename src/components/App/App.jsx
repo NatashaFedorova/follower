10f41bdb3aeb2c3192200407c066a999a96b1a26
Наташа Федорова <98808643+NatashaFedorova@users.noreply.github.com>
2023-05-31 14:13:59 +0300

    deleted not used code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,10 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { lazy } from 'react';
 import SharedLayout from '../SharedLayout';
 import { Background } from '../../constants/componentsDefaultStyle';
 
 const HomePage = lazy(() => import('../../pages/HomePage'));
 const TweetsPage = lazy(() => import('../../pages/TweetsPage'));
-
-const RouteRedirection = ({ component: Component, redirectTo }) => {
-  return <Navigate component={<Component />} redirectTo={redirectTo} />;
-};
 
 const App = () => {
   return (
@@ -26,8 +21,3 @@ const App = () => {
 };
 
 export default App;
-
-RouteRedirection.propTypes = {
-  component: PropTypes.func,
-  redirectTo: PropTypes.string,
-};
