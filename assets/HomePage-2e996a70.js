2ad11abe178577fe285309189089e2bdaf67da6c
Наташа Федорова <98808643+NatashaFedorova@users.noreply.github.com>
2023-05-31 13:28:22 +0300

    Updates

--- assets/HomePage-2e996a70.js
+++ assets/HomePage-2e996a70.js
@@ -1,3 +1,3 @@
-import{s as t,j as e}from"./index-e24d367d.js";const s=t.h1`
+import{s as t,j as e}from"./index-3eadbe6f.js";const s=t.h1`
   text-align: center;
 `,o=t.img``,n=()=>e.jsxs(e.Fragment,{children:[e.jsx(o,{src:"./images/decore-img.svg",alt:"logo",width:"62",height:"62"}),e.jsx(s,{children:"Welcome!"})]}),m=()=>e.jsx(n,{});export{m as default};
